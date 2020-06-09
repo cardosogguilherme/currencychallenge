1b3e4d288964a0897fab49971df5c2760f1ad639
Guilherme Cardoso <cardosogguilherme@gmail.com>
2020-06-09 23:37:57 +1200

    initial commit

---
+++ app/src/main/java/com/guilhermecardoso/currencychallenge/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.guilhermecardoso.currencychallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
