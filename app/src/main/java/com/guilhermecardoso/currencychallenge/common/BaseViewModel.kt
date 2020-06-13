6ab9ffa4c8dfe3317ca131c524274995c02a3ad6
Guilherme Cardoso <cardosogguilherme@gmail.com>
2020-06-13 23:47:21 +1200

    Adding base VM and extensions

--- app/src/main/java/com/guilhermecardoso/currencychallenge/common/BaseViewModel.kt
+++ app/src/main/java/com/guilhermecardoso/currencychallenge/common/BaseViewModel.kt
@@ -1,2 +1,13 @@
 package com.guilhermecardoso.currencychallenge.common
 
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+open class BaseViewModel : ViewModel() {
+
+    val empty = MutableLiveData<Boolean>().apply { value = false }
+
+    val dataLoading = MutableLiveData<Boolean>().apply { value = false }
+
+    val toastMessage = MutableLiveData<String>()
+}
--- app/src/main/java/com/guilhermecardoso/currencychallenge/common/Extensions.kt
+++ app/src/main/java/com/guilhermecardoso/currencychallenge/common/Extensions.kt
@@ -1,2 +1,19 @@
 package com.guilhermecardoso.currencychallenge.common
 
+import android.view.View
+
+fun Any.TAG(): String = this.javaClass.simpleName
+
+fun View?.visibleIf(condition: Boolean, shouldBeGone: Boolean = false) {
+    when {
+        condition -> {
+            this?.visibility = View.VISIBLE
+        }
+        shouldBeGone -> {
+            this?.visibility = View.GONE
+        }
+        else -> {
+            this?.visibility = View.INVISIBLE
+        }
+    }
+}
